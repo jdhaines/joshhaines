f67952bb5c9db85564ac6e560251640c5322e7c0
Josh Haines <Josh@JoshHaines.com>
2022-02-25 17:30:07 -0500

    fix(build): remove preact for now so build runs

--- next.config.js
+++ next.config.js
@@ -33,9 +33,9 @@ module.exports = withBundleAnalyzer({
     if (!dev && !isServer) {
       // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+        // react: 'preact/compat',
+        // 'react-dom/test-utils': 'preact/test-utils',
+        // 'react-dom': 'preact/compat',
       })
     }
 
