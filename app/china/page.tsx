ddeae5ec265c06197d18d36f4e4157c76206528b
Josh Haines <Josh@JoshHaines.com>
2025-01-10 08:24:13 -0500

    refactor(china): remove branding from slider, add text and rest of pictures

--- app/china/page.tsx
+++ app/china/page.tsx
@@ -13,25 +13,38 @@ export default function China() {
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             Below, you can check out a few photos from our China trip from late 2024 and early 2025!
+            <br />
+            <br />
+            We visited Tianjin China for about 1.5 weeks, then flew to Chengdu in the Sichuan
+            Province for some amazing food and to see the Pandas. We went back to Tianjin for a
+            couple days and then to Beijing to explore the city and visit the Great Wall of China.
+            Finally, on our way out, we had almost a full day to explore Tokyo. It was a great trip
+            and we can't wait to go back!
           </p>
         </div>
         <NoSsr>
           <div className="container py-12">
             <div className="-m-4 flex flex-wrap">
-              <script
-                src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js"
-                async
-              ></script>
+              <script src="/static/js/embed-ui.min.js" async></script>
               <div
                 className="pa-gallery-player-widget"
                 style={{ width: '100%', height: '480px', display: 'none' }}
                 data-link={'https://photos.app.goo.gl/5612mwMLxiFPMjoY6'}
-                data-title={'Public China Photos'}
+                data-title={''}
                 data-background-color={'#111827'}
                 data-description={'2 new items added to shared album'}
               >
-                <object data="https://lh3.googleusercontent.com/pw/AP1GczMqLnyw3kg9WiPNCAYrICit7r-W24HDko6_jOn7NR_9PpuXBY-Yvo2TWneX077Mawkf6J7rQJwev2FeXTpsl5pjQOG4FOpf4hoKIN3lutxkcMhduj0a=w1920-h1080"></object>
-                <object data="https://lh3.googleusercontent.com/pw/AP1GczP6ej_rncDEPH4Jk6VlZPn0d26i_xv2uU8Fuu8fn9k9vFmNiEniy7H7n8yUmfdHtkwhkxbdcARVKB2UHXj_R9MoBekXUF167Jtj3gj9zBv_fOcAIQFu=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczPDPkfSgcMTNduOSgjmdVPd3SMuVojz9MaOZ64e4PZl7cvG3e8-V_vcJXD4z8Fe9UKh8TvJ72mP7sS4SqZQd7ptaGw3EyGQCchjQK9uK6kZrv_IpDRE=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczOkWYr6QCBcV4t1PG7JYyx7cLpLqoj0mzw84iSwBEuwR-cD71N3chzVVp25OkdsUTBtqeHomCcBa0knT_Ty2Df3OuJ4eUUD5kMjoDVV9TGFyuYxGCxr=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczNFpANd7HcdE-dWAVgmA7U_DNmoFfEaTwGMXUeuG5Gd6eMkW_2BBWAd6lkB0-H1k99ra13cG2Lu_pw0BIGZ6rCI1T3k5t544Yqd_uZtSF7qjKYhK0aO=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczNeLF5QVndi7hf4Xvtg0PpqNGeGcfU31pF8J6uTpXSY39uQ9lyad_fyd1t9n5cb8uxZf6nNNCo4EM79Bn-Xrn3LgrNpyutwpSsnmRabMIAgabPoPqRh=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczOTNIr5kRBn3nDbZUik_5IJ1sXypTJLlwIn-fQjk35WPXxhmio0ZrB-NonrMq2SRoWtFH7SrFcedoC02mOy__Gxd53upl9079IEdTIv_PSER9CXkMFf=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczPJLzWOqrEd0Hm1c_xXielnRm6YZk9aMxIygkBR6-FrHasdgC5oBT-ZwW9V4PjgiIxjQwTTVlVB5dvZYEThcTPfQdNBXU0fWeHDxeC5MpfrTTcKXeXA=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczOAtR0ytKWP73E6pEEoB0WK2B9-jxBzmbBYo5vvXPn8FzA2BKVpA_yfeBkYmFZ1dY5kR4oL9x-ky6NxxwRKyvp2Kg3xoyK8-zTVT0nqyn1MSbbqDfUX=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczMZXhaqy-00wH93IYqiFWD-jCfBhmO3D1X1jP90awi3UV4IZDOTwS95j8I4W8rwtL_jV-XQ6ltxHILsPN2geo439lBL8SQkiYh8POeHt0Nz_pr2yL9z=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczP8PiwnuRuB5I18iaszy7u1hi52wfI80FGhOBUG7t-c7MRQ2C7zJiM3-JZQx1LMtTDpwjGGbcdpsuewNlF60Q_eYmiZxsmhCKE27lMxqQHlQTSYlXd7=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczOGJdMsDe0r__CxEI-0rD-XYWMxvh6h142yBPpfPd_JBtOx0-pwwjtKaqncH5rN6dcmgmnm9VEH_w8kWGXEhLArfJvLEx77Vw1eGsmg9i_4WNAVQsEf=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AP1GczPBaFF7cge_vFZwK0GhAO6gnpenEiDndszJMZGMEUY2oenViusznGLxGjVYzbhIXixzoU3Cp3H6L-YgWIMIJwwYHg_VUzkOG_7j4dhFL1Ddi-D0AcTu=w1920-h1080"></object>
               </div>
             </div>
           </div>
